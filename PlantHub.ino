8353ec71d0e8f6cbcc377877608d1d605ab3522d
Nisarg Shah <nshah9856@users.noreply.github.com>
2019-11-28 09:42:58 -0800

    Add files via upload
    Works for now

--- PlantHub.ino
+++ PlantHub.ino
@@ -1,15 +1,13 @@
 
 #include <Wire.h>
-#include "rgb_lcd.h"
+//#include "rgb_lcd.h"
 
 #define SoilMoisturePin A2
 #define SoilMoisturePower 6
 #define LightSensorPin A0
 #define TemperatureSensorPin A3
 
-rgb_lcd lcd;
-bool flag1 = LOW;
-byte x;
+//rgb_lcd lcd;
 
 const int wetThreshold = 400;
 const int dryThreshold = 250;
@@ -58,67 +56,41 @@ int readSoil() {
   return val;//send current moisture value
 }
 
-void receiveEvent(int howmany){  
-  x = Wire.read();  
-  flag1 = HIGH;  
-  //Serial.print(x, HEX);
-}
-
-void sendEvent(int howmany){  
-  Wire.write(0x45);
-}
-
 void setup() {
-  Serial.begin(115200);  
-  Wire.begin(0x08);  
-  pinMode(13, OUTPUT); 
-  Wire.onReceive(receiveEvent);  
-  Wire.onRequest(sendEvent);
-  
-  lcd.begin(16, 2);
+  Serial.begin(9600);               
 
-  lcd.createChar(1, water);
-  lcd.createChar(2, sun);
-  lcd.createChar(3, temp);
+//  lcd.begin(16, 2);
+//
+//  lcd.createChar(1, water);
+//  lcd.createChar(2, sun);
+//  lcd.createChar(3, temp);
   pinMode(SoilMoisturePower, OUTPUT);         //Set D6 as an OUTPUT
   digitalWrite(SoilMoisturePower, LOW);       //Turn it Off when not in use
-
-  lcd.setCursor(0, 0);
-  lcd.write(byte(1));
-  lcd.setCursor(9, 0);
-  lcd.write(byte(2));
-  lcd.setCursor(0, 1);
-  lcd.write(byte(3));
-
-
+//
+//  lcd.setCursor(0, 0);
+//  lcd.write(byte(1));
+//  lcd.setCursor(9, 0);
+//  lcd.write(byte(2));
+//  lcd.setCursor(0, 1);
+//  lcd.write(byte(3));
+  delay(500); //Just to make sure setup completed
 }
 
-char soilBuf[4];
-char lightBuf[4];
-char temperatureBuf[4];
-
+/*
 void loop() {
 
-  if (flag1 == HIGH)  {    
-    Serial.println(x, HEX);    
-    flag1 = LOW;  
-  }  
-  digitalWrite(13, !digitalRead(13));  
-  delay(1000);
-
-  
   int soilMoistureLevel= readSoil();
-  lcd.setCursor(2, 0);
-  sprintf(soilBuf, "%-3d", soilMoistureLevel);
-  lcd.print(soilBuf);
+//  lcd.setCursor(2, 0);
+//  sprintf(soilBuf, "%-3d", soilMoistureLevel);
+//  lcd.print(soilBuf);
 
   Serial.print("Soil Moisture level: ");
   Serial.println(soilMoistureLevel);
 
   int lightSensorValue = analogRead (LightSensorPin);
-  lcd.setCursor(12, 0);
-  sprintf(lightBuf, "%-3d", lightSensorValue);
-  lcd.print(lightBuf);
+//  lcd.setCursor(12, 0);
+//  sprintf(lightBuf, "%-3d", lightSensorValue);
+//  lcd.print(lightBuf);
 
   Serial.print("Light:");
   Serial.println(lightSensorValue);
@@ -130,12 +102,45 @@ void loop() {
 
   float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
   dtostrf(temperature, 4, 1, temperatureBuf);
-  lcd.setCursor(2, 1);
-    
-  lcd.print(temperatureBuf);
-  lcd.print("C");
+//  lcd.setCursor(2, 1);
+//    
+//  lcd.print(temperatureBuf);
+//  lcd.print("C");
   Serial.print("Temperature:");
   Serial.println(temperature);
   
-   delay(1000);
+  delay(1000);
+}
+*/
+
+int getSoil(){
+  int l = readSoil();
+  Serial.print("Soil moisture: ");
+  Serial.println(l);
+  return l;
+}
+
+float getTemp(){
+  int temperatureSensorValue = analogRead (TemperatureSensorPin);
+  float R = 1023.0/temperatureSensorValue-1.0;
+  R = R0*R;
+  float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15;
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  return temperature;
+}
+
+int getLuminosity(){
+  int lightSensorValue = analogRead (LightSensorPin);
+  Serial.print("Luminosity: ");
+  Serial.println(lightSensorValue);
+  return lightSensorValue;
+}
+
+
+void loop(){
+  int soilMoisture = getSoil();
+  float tempValue = getTemp();
+  int luminosityValue = getLuminosity();
+  delay(1000);
 }
